24f5155d670171d7e872bc41123892d3877bdb64
Naveen Bhusare <79650422+Naveen-7000@users.noreply.github.com>
2023-01-19 14:25:49 +0530

    setup done

--- models/user.js
+++ models/user.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
 const bcrypt = require("bcryptjs");
+const JWT = require("jsonwebtoken");
+const crypto = require("crypto");
 // here in required i am using array to tell that if its true then go ahead
 // if not then pass this message to validator or show this message
 const userSchema = new mongoose.Schema({
@@ -45,9 +47,37 @@ const userSchema = new mongoose.Schema({
   },
 });
 
-//  encrypt password before save
+//  encrypt password before save -HOOKS
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+  this.password = await bcrypt.hash(this.password, 10);
+});
 
+// validating the password using method with the passedd pass by user to DB pass
+userSchema.methods.IsValidatedPassword = async function(userEnteredPassword){
+   return await bcrypt.compare(userEnteredPassword,this.password);
+}
 
+// JWT Token a method to create and return the token
+userSchema.methods.getJwtToken = async function(){
+  JWT.sign({id:this._id,},process.env.JWT_SECRET,{
+    expiresIn: process.env.JWT_EXPIRY,
+  })
+}
+
+// generate forgot password token (simple string )
+userSchema.methods.getForgotPasswordToken = async function(){
+  // generate a long and random string 
+   const forgotToken  = crypto.randomBytes(20).toString('hex');
+  //  getting a hash  - make sure to get a hash on backend
+   this.forgotPasswordToken = crypto.createHash('sha256').update(forgotToken).digest('hex');
+
+  //  time of token to be added in DB
+  this.forgotPasswordExpiry = Date.now() + 20*60*1000;
+  return forgotToken;
+}
 
 
 module.exports = mongoose.model("User", userSchema);
--- routes/home.js
+++ routes/home.js
@@ -2,10 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 // bring method from controller
-const {home, homeDummy, check} = require('../controller/homeContoller');
+const {home, homeDummy} = require('../controller/homeContoller');
 
 router.route('/').get(home)
 router.route('/dummy').get(homeDummy)
-router.route('/check').get(check);
 
 module.exports = router;
